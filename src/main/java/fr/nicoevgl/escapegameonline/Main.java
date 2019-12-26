128bedffa285c915ad6d1ae175c40eb1d5de50f2
Nico Evgl <nico.evangelista33@gmail.com>
2019-12-26 14:08:59 +0000

    Class "Main" : permet de lancer l'application, affiche le menu principal et appel la classe "Game".

--- src/main/java/fr/nicoevgl/escapegameonline/Main.java
+++ src/main/java/fr/nicoevgl/escapegameonline/Main.java
@@ -1,9 +1,53 @@
 package fr.nicoevgl.escapegameonline;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        int nbChoice;
+        do {
+            System.out.println("Bienvenue dans EscapeGame Online !" + "\n");
 
+            System.out.println("Choisissez votre mode de jeu");
+            System.out.println("1 - Mode Challenger");
+            System.out.println("2 - Mode Défenseur");
+            System.out.println("3 - Mode Duel");
+            System.out.println("4 - Quitter");
+
+            int nbMode = 0;
+            Scanner scMode = new Scanner(System.in);
+            Game game = new Game();
+
+            try {
+                nbMode = scMode.nextInt();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Veuillez saisir une valeur correcte.");
+            }
+            do {
+                game.runSelectedMode(nbMode);
+
+                Scanner scChoice = new Scanner(System.in);
+                System.out.println("Fin de la partie.");
+                System.out.println("1 : Rejouer  2 : Retour au menu principal  3 : Quitter");
+                nbChoice = scChoice.nextInt();
+                switch (nbChoice) {
+                    case 1:
+                        System.out.println("Vous avez choisis de rejouer");
+                        break;
+                    case 2:
+                        System.out.println("Retour au menu principal...");
+                        break;
+                    case 3:
+                        System.out.println("Au revoir...");
+                        break;
+                    default:
+                        System.out.println("Je n'ai pas compris...");
+                        break;
+                }
+            }while (nbChoice == 1);
+        } while (nbChoice == 2 || nbChoice > 3);
     }
 
 
